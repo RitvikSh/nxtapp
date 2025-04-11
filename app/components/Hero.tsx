b312879c0643beba62b477500eb7974a1d360245
RitvikSh <praveensiarma@gmail.com>
2025-04-11 04:49:50 +0000

    navbar done

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,21 @@
+'use client'
+
+import Image from 'next/image';
+import Robot from '../../public/download-removebg-preview.png'
 export default function Hero(){
-    return <>
-    this is the hero section
-    </>
+const test = () =>{
+alert("work")
+
+}
+return <div className='pt-16'>
+    
+   <h1 className='text-center text-[32px] text-[#36485C] font-medium leading-[40px]'>
+   Start monitoring your website like a pro
+   </h1>
+   <Image className='mx-auto pt-5' alt="hero" src={Robot} />
+   <div className='text-center'>
+    <button className='bg-[#36485C] text-white p-2 rounded mx-5'onClick={test}>Try For Free</button>
+    <button className='bg-[#36485C] text-white p-2 rounded mx-5'>View Pricing</button>
+   </div>
+    </div>
 }
